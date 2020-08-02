a8f7d1c70e33dc937e2c893dc2a0a208143b9eb3
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-08-02 10:58:50 -0500

    update env.mk to default python 3.7

--- env.mk
+++ env.mk
@@ -4,5 +4,5 @@ DOCKER_USERNAME?=octoprint
 IMAGE?=octoprint/octoprint
 PLATFORM?=arm64
 DOCKERFILE_LOCATION?="./Dockerfile"
-PYTHON_BASE_IMAGE?=2.7-slim-buster
-PLATFORMS="linux/arm64,linux/amd64"
+PYTHON_BASE_IMAGE?=3.7-slim-buster
+PLATFORMS="linux/arm64,linux/amd64,linux/arm/v7"
