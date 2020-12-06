1d83247d9fb1db2c3674563cb1f506896b01544d
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-12-06 12:05:14 -0600

    preconfigure docker specific settings
    closes #90

--- env.mk
+++
@@ -1,8 +0,0 @@
-# Configs for build and test
-REGISTRY?=docker.io
-DOCKER_USERNAME?=octoprint
-IMAGE?=octoprint/octoprint
-PLATFORM?=arm64
-DOCKERFILE_LOCATION?="./Dockerfile"
-PYTHON_BASE_IMAGE?=3.8-slim-buster
-PLATFORMS="linux/arm64,linux/amd64,linux/arm/v7"
